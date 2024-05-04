5891a0cf16db34de2a7838b0af274f94371b86f6
st3v1sh <st3v1sh@gmail.com>
2024-05-04 13:30:29 -0400

    ignore advancements; just doesn't work

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ function fillTemplate(template, data) {
     return Object.keys(data).reduce((accumulator, keyword) => accumulator.replace(new RegExp(`{{${keyword}}}`, 'g'), data[keyword]), template);
 }
 
-function buildRewardList(rewards) {
-    return rewards.map(reward => `"extracutter:${reward}"`).join(', ');
-}
-
 const output = fs.createWriteStream('extracutter.zip');
 const archive = archiver('zip', {
     zlib: { level: 9 }
@@ -29,41 +25,28 @@ archive.pipe(output);
 // Set up mcmeta.
 archive.file('templates/pack.mcmeta', { name: 'pack.mcmeta' });
 
-// Templates.
-const advancementTemplate = fs.readFileSync('templates/advancements.txt', 'utf8');
+// Template.
 const recipeTemplate = fs.readFileSync('templates/recipes.txt', 'utf8');
 
 // Set up wooden slab recipes.
 woodPlanks.forEach(([name, suffix]) => {
     const plankName = suffix.length === 0 ? name : `${name}_${suffix}`;
-    const recipeFile = `${name}_slab_from_${plankName}_extracutter.json`;
-    const advancement = fillTemplate(advancementTemplate, { item: plankName, recipes: buildRewardList([recipeFile]) });
+    const recipeFile = `${name}_slab_from_${plankName}_extracutter`;
     const recipe = fillTemplate(recipeTemplate, { item: plankName, result: `${name}_slab`, count: '2' });
-
-    archive.append(advancement, { name: `data/extracutter/advancements/recipes/building_blocks/${recipeFile}` });
-    archive.append(recipe, { name: `data/extracutter/recipes/${recipeFile}` });
+    archive.append(recipe, { name: `data/extracutter/recipes/${recipeFile}.json` });
 });
 
 // Set up isomorphisms.
 isomorphisms.forEach(isomorphism => {
     isomorphism.forEach(itemA => {
-        // Advancements.
-        const recipeFiles = isomorphism
-            .map(itemB => `${itemB}_from_${itemA}_extracutter.json`)
-            .filter(recipeFile => recipeFile !== `${itemA}_from_${itemA}_extracutter.json` && !existingRecipes
-                .some(([input, output]) => recipeFile === `${output}_from_${input}_extracutter.json`));
-        if (recipeFiles.length > 0) {
-            const advancement = fillTemplate(advancementTemplate, { item: itemA, recipes: buildRewardList(recipeFiles) });
-            archive.append(advancement, { name: `data/extracutter/advancements/recipes/building_blocks/${itemA}_extracutter.json` });
-        }
-
-        // Recipes.
         isomorphism.forEach(itemB => {
             if (itemA === itemB || existingRecipes.some(([input, outpit]) => input === itemA && outpit === itemB)) {
                 return;
             }
+
+            const recipeFile = `${itemB}_from_${itemA}_extracutter`;
             const recipe = fillTemplate(recipeTemplate, { item: itemA, result: itemB, count: '1' });
-            archive.append(recipe, { name: `data/extracutter/recipes/${itemB}_from_${itemA}_extracutter.json` });
+            archive.append(recipe, { name: `data/extracutter/recipes/${recipeFile}.json` });
         });
     });
 });
